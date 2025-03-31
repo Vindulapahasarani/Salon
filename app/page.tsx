b1f1b10fb023127e0228e88afb7dd94a7c611ea1
Vindula Pahasarani <85609775+Vindulapahasarani@users.noreply.github.com>
2025-03-31 11:03:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
-
-import Home from "@/components/Home/Home";
 import React from "react";
+import Home from "@/components/Home/Home";
+import AboutUs from "@/components/About Us/Aboutus";
+import Services from "@/components/Services/Services";
+import Booking from "@/components/Booking/Booking";
+import Contact from "@/components/Contact/Contact";
 
 const HomePage = () => {
   return ( 
     <div>
+      <section id="home">
         <Home />
+      </section>
       
-        
+      <section id="about">
+        <AboutUs />
+      </section>
+
+      <section id="services">
+        <Services />
+      </section>
+
+      <section id="booking">
+        <Booking />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
     </div>
   );
 };
