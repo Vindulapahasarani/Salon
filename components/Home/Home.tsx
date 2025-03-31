3549372b97169b441486b73a7462e25a3b7d233b
Vindula Pahasarani <85609775+Vindulapahasarani@users.noreply.github.com>
2025-03-31 10:35:33 +0530

    Update Home.tsx

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Hero from './Hero/Hero';
-import Footer from './Footer/Footer';
-import Services from '../Services/Services';
-import Aboutus from '../About Us/Aboutus';
-import Booking from '../Booking/Booking';
 
 const Home = () => {
   return (
     <div className='overflow-hidden '>
-      <Hero />
-      <Aboutus />
-      <Services />
-      <Booking />
-      <Footer />
+     <Hero />
 
     </div>
   );
