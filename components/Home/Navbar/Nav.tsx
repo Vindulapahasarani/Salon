b79775818d0c07c60962f5185356c6c6b9a7dcb4
Vindula Pahasarani <85609775+Vindulapahasarani@users.noreply.github.com>
2025-03-31 11:04:47 +0530

    Update Nav.tsx

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -6,55 +6,58 @@ import { HiBars3BottomRight } from 'react-icons/hi2';
 import { TbAirBalloon } from 'react-icons/tb';
 
 type Props = {
-  openNav:()=>void
+  openNav: () => void;
 };
 
-const Nav = ({openNav}:Props) => {
-      const [navBg,setNavBg]=useState(false);
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
 
-      useEffect(()=>{ 
-          const handler = ()=>{
-            if(window.scrollY>=90) setNavBg(true);
-            if(window.scrollY<90) setNavBg(false);
-          };
-          window.addEventListener('scroll',handler);
-          return()=> window.removeEventListener('scroll',handler);
-      },[]);
+  useEffect(() => { 
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      else setNavBg(false);
+    };
+    window.addEventListener('scroll', handler);
+    return () => window.removeEventListener('scroll', handler);
+  }, []);
 
-    return (
-      <div className={`bg-blue-950 ${navBg ? 'shadow-md' : 'fixed'} transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
-        <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
-          {/* LOGO */}
-            <div className='flex items-center space-x-2'>
-              <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center'>
-                <TbAirBalloon className='w-6 h-6 text-white'/>
-              </div>
-              <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>
-                Saloon
-              </h1>
-            </div>
-            {/* NavLinks */}
-            <div className='hidden lg:flex items-center space-x-10'>
-              {navLinks.map((link) => (
-                <Link href={link.url} key={link.id}>
-                  <p className='relative text-white text-base font-medium w-fit block after:block after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 hover:after:scale-x-100 after:transition-all after:duration-200'>
-                    {link.label}
-                  </p>
-                </Link>
-              ))}
-            </div>
-
-            {/* buttons */}
-            <div className='flex items-center space-x-4'>
-              <button className='md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg'>
-                Book Now
-              </button>
-              {/* Burger Menu */}
-              <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
-            </div>
+  return (
+    <div className={`bg-blue-950 ${navBg ? 'shadow-md' : 'fixed'} transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
+      <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
+        
+        {/* LOGO */}
+        <div className='flex items-center space-x-2'>
+          <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center'>
+            <TbAirBalloon className='w-6 h-6 text-white' />
+          </div>
+          <h1 className='text-xl md:text-2xl text-white uppercase font-bold'>
+            Salon
+          </h1>
         </div>
+
+        {/* Nav Links */}
+        <div className='hidden lg:flex items-center space-x-10'>
+          {navLinks.map((link) => (
+            <Link href={link.url} key={link.id} scroll={true}>
+              <p className='relative text-white text-base font-medium w-fit block after:block after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 hover:after:scale-x-100 after:transition-all after:duration-200'>
+                {link.label}
+              </p>
+            </Link>
+          ))}
+        </div>
+
+        {/* Buttons */}
+        <div className='flex items-center space-x-4'>
+          <button className='md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg'>
+            Book Now
+          </button>
+          {/* Burger Menu */}
+          <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-white lg:hidden' />
+        </div>
+
       </div>
-    );
+    </div>
+  );
 };
 
 export default Nav;
